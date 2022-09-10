6f5d45a393509c4b2ed968057e4a4f023647b3fb
Neuromancer42 <chenyifan_1997@hotmail.com>
2022-09-11 02:00:17 +0800

    fix bugs in Call-Graph resolving

---
+++ com.neuromancer42.tea.core/src/com/neuromancer42/tea/core/analyses/IDebuggable.java
@@ -0,0 +1,5 @@
+package com.neuromancer42.tea.core.analyses;
+
+public interface IDebuggable {
+    String toDebugString();
+}
--- com.neuromancer42.tea.program.cdt/src/com/neuromancer42/tea/program/cdt/internal/cfg/ICFGNode.java
+++ com.neuromancer42.tea.program.cdt/src/com/neuromancer42/tea/program/cdt/internal/cfg/ICFGNode.java
@@ -1,5 +1,6 @@
 package com.neuromancer42.tea.program.cdt.internal.cfg;
 
-public interface ICFGNode {
-    String toDebugString();
+import com.neuromancer42.tea.core.analyses.IDebuggable;
+
+public interface ICFGNode extends IDebuggable {
 }
